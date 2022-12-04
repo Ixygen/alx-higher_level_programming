c480cc50de4e2ff2404677c6c832ea4adf223b4a
Ixygen <naivenyabinya@gmail.com>
2022-12-03 18:06:57 -0800

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
-def uppercase(str):
-    for uppercase in range(ord('A'), ord('Z')+1):
-            print(uppercase)
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best school 98 Battery Street")
