7592d697a3f5bb0482e5411549eb5db2d9e5d65f
Ixygen <naivenyabinya@gmail.com>
2022-12-05 22:02:41 +0300

    '1'

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    my_list = list()
+    my_list = []
+    if idx == -idx:
+        return None
+    elif idx != my_list:
+        return None
+    else:
+        return my_list
+
+
+element_at(my_list, idx)
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -1,1 +1,3 @@
 0. Function that prints all integers of a list
+1. Function that retirves an element from a list like in C
+2.
