8f0635650ed9d6dba3742a2bf41b672a5fadcecf
ixygen <naivenyabinya@gmail.com>
2022-12-09 07:38:15 +0300

    print lowercase using ord

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for lowercase in range(ord('a'), ord('z')+1):
     if lowercase != 'e' and lowercase != 'q':
-        print("{:c}".format(lowercase), end="")
+        print(f"{chr(lowercase)}",end="")
