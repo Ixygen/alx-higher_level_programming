1feadb7fa5fc4a9d64b03887b632542d4f5337da
ixygen <naivenyabinya@gmail.com>
2022-12-09 06:03:48 +0300

    3

---
+++ 102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    bytecode = dis.Bytecode(magic_calculation)
+    for item in bytecode:
+        print(item.opname)
+
+
+magic_calculation(a, b)
---
+++ 8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = ("python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clever syntax").split()
+str = str[5] + ' ' + str[6] + ' ' + str[12] + ' ' + str[0]
+print(str)
